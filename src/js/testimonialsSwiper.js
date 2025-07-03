6459541869234b26af4286b0d52dbeecf3d0f33b
Jango <kkuzmenko1992@gmail.com>
2025-07-03 01:20:26 -0230

    testimonials mobile completed

---
+++ src/js/testimonialsSwiper.js
@@ -0,0 +1,34 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+let reviewsSwiper = null;
+
+function initReviewsSwiper() {
+  const breakpoint = 1200;
+  const isMobile = window.innerWidth < breakpoint;
+
+  if (isMobile && !reviewsSwiper) {
+    const prevSlide = document.querySelector('.oh-button-left');
+    const nextSlide = document.querySelector('.oh-button-right');
+
+    reviewsSwiper = new Swiper('.test-swiper', {
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      spaceBetween: 40,
+      mousewheel: true,
+      loop: true,
+      navigation: {
+        nextEl: '.oh-button-right',
+        prevEl: '.oh-button-left',
+      },
+    });
+  }
+
+  if (!isMobile && reviewsSwiper) {
+    ourHelpSwiper.destroy(true, true);
+    ourHelpSwiper = null;
+  }
+}
+
+window.addEventListener('load', initReviewsSwiper);
+window.addEventListener('resize', initReviewsSwiper);
